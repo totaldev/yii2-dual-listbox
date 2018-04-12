5445126e4a0083e4fd9a2b43e0a514e2db209445
Vadim <4ws@mail.ru>
2018-04-12 20:26:01 +0300

    Initial commit

---
+++ Asset.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vk
+ * Date: 12.04.18
+ * Time: 21:10
+ */
+
+namespace totaldev\yii\dualListBox;
+
+use yii\web\AssetBundle;
+
+class Asset extends AssetBundle
+{
+    public $css = [];
+    public $js = [];
+}
---
+++ DualListBox.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vk
+ * Date: 12.04.18
+ * Time: 21:10
+ */
+
+namespace totaldev\yii\dualListBox;
+
+use yii\base\Widget;
+
+class DualListBox extends Widget
+{
+
+}
